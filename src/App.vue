f71e0d5aa66f28c3008901abd6c10f06b852d302
camillapalombi <camilla.palombi14@gmail.com>
2022-04-01 17:07:19 +0200

    Inizio Header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <!--HEADER-->
+    <header-maree />
 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//IMPORT
+import HeaderMaree from '../src/components/HeaderMaree.vue';
 
+//EXPORT
 export default {
   name: 'App',
+  components: {
+    HeaderMaree,
+  }
 }
 </script>
 
 <style lang="scss">
-
+//import tutti gli stili
+@import './assets/style/style';
 </style>
---
+++ src/components/HeaderMaree.vue
@@ -0,0 +1,101 @@
+<template>
+  <header>
+
+      <!-- HEADER LEFT / LOGO -->
+      <div class="header-left">
+          <img class="maree-logo" src="./../assets/img/logo-sidearea-1.png" alt="Marèe Logo">
+      </div>
+
+      <!-- HEADER RIGHT / NAV -->
+      <nav class="header-right">
+
+          <ul>
+              <li v-for="link in arrNavLinks" :key="link"> 
+                  <a href="#!"> {{ link.link}}</a>
+              </li>
+
+              <li>
+                  <a href="#!">
+                      <img class="svg-menù" src="./../assets/img/svg-1-dado.svg" alt="Menù link">
+                  </a>
+              </li>
+
+          </ul>
+
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+name: 'HeaderMaree',
+data () {
+    return {
+        arrNavLinks: [
+            {
+                link: 'HOME'
+            },
+            {
+                link: 'PAGES'
+            },
+            {
+                link: 'PORTFOLIO'
+            },
+            {
+                link: 'BLOG'
+            },
+            {
+                link: 'SHOP'
+            },
+            {
+                link: 'ELEMENTS'
+            },
+        ]
+    }
+}
+}
+</script>
+
+<style scoped lang="scss">
+//import variabili colori
+@import '../assets/style/partials/variables';
+
+header {
+    height: 4rem;
+    background-color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .header-left {
+        width: 50%;
+
+        .maree-logo {
+            cursor: pointer;
+            width: 100px;
+        }
+    }
+
+    .header-right {
+        width: 40%;
+
+        ul {
+            display: flex;
+            justify-content: space-around;
+            list-style: none;
+
+            a {
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+                font-size: 12px;
+            }
+
+            .svg-menù {
+                width: 16px;
+                vertical-align: middle;
+            }
+        }
+    }
+}
+</style>
