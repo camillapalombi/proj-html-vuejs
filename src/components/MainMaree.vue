2e4ec82e1230981bcc3a8039e69301dfe5fc9292
camillapalombi <camilla.palombi14@gmail.com>
2022-04-03 17:40:39 +0200

    Inizio Main Section Two

--- src/components/MainMaree.vue
+++ src/components/MainMaree.vue
@@ -2,17 +2,21 @@
   <main>
       <!--SECTION 1 -->
       <main-section-one />
+      <!--SECTION 2 -->
+      <main-section-two />
 
   </main>
 </template>
 
 <script>
 import MainSectionOne from '@/components/MainSectionOne.vue'
+import MainSectionTwo from '@/components/MainSectionTwo.vue'
 
 export default {
 name: 'MainMaree',
 components: {
     MainSectionOne,
+    MainSectionTwo,
 }
 }
 </script>
@@ -21,6 +25,7 @@ components: {
 //import variabili colori
 @import '../assets/style/partials/variables';
 
+//Google font
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,600&display=swap');
 
 </style>
---
+++ src/components/MainSectionTwo.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="section-two">
+
+      <div class="container-image" v-for="image in arrPhotos" :key="image">
+          <img :src="require('./../../public/img/' + image.image)" alt="">
+      </div>
+
+      <div class="container-icons chiocciolina">
+              <img src="./../../public/img/svg-4-chiocciolina-bianca.svg" alt="Chiocciolina bianca">
+          </div>
+
+          <div class="container-icons carrello">
+              <img src="./../../public/img/svg-6-carrello.svg" alt="Carrello">
+          </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+name: 'MainSectionTwo',
+data () {
+    return {
+        arrPhotos: [
+            {
+                image: 'h-2-port-img-1.jpg',
+            },
+            {
+                image: 'h-2-port-img-2.jpg',
+            },
+            {
+                image: 'h-2-port-img-3.jpg',
+            },
+            {
+                image: 'h-2-port-img-4.jpg',
+            },
+            {
+                image: 'h-2-port-img-5.jpg',
+            },
+            {
+                image: 'h-2-port-img-6.jpg',
+            },
+        ]
+    }
+}
+}
+</script>
+
+<style scoped lang="scss">
+//import variabili colori
+@import '../assets/style/partials/variables';
+
+.section-two {
+    height: 60rem;
+    margin-top: 80px;
+    background-color: aquamarine;
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+}
+.container-image {
+    width: calc(100% / 3);
+    height: 50%;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+}
+.container-icons {
+    position: absolute;
+    right: 0px;
+    width: 25px;
+    height: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    img {
+        width: 15px;
+    }
+}
+.chiocciolina {
+    top: 80px;
+    background-color: #ef2853;
+}
+.carrello {
+    top: 110px;
+    background-color: white;
+}
+</style>
